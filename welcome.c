1e75b298e83a9c83dd78295077f522bf82bfe51c
Alan Thomas Shaji <50659499+Captain-shaji-stark@users.noreply.github.com>
2020-10-01 21:33:05 +0530

    pinnem Udpate welcome.c

--- welcome.c
+++ welcome.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 void main(){
+  char name[20];
   printf("Welcome bois and gals.");
   printf("Welcome to HacktoberFest 2020!!!");
+  printf("What's your name??");
+  gets(name);
+  printf("Hi, %s", name);
 }
