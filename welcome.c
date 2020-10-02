d807ec4fdd81271b94fda1141d14e2aff0b1e120
meenu-km <62182028+meenu-km@users.noreply.github.com>
2020-10-02 12:52:50 +0530

    Update welcome.c

--- welcome.c
+++ welcome.c
@@ -1,12 +1,7 @@
 #include<stdio.h>
 void main(){
-  char name[20];
   printf("Welcome bois and gals.");
   printf("Welcome to HacktoberFest 2020!!!");
-
-  printf("What's your name??");
-  gets(name);
-  printf("Hi, %s", name);
   printf("Ivide entha karyam ^_^ ");
-
+  printf("verthe vannatha")
 }
